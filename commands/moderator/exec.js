72b5cbffd497699846e1e9cd05d5bd0b45151d23
nosnowowie93347 <nosnowowie@Gmail.com>
2021-04-06 11:39:33 -0700

    Update exec.js

--- commands/moderator/exec.js
+++ commands/moderator/exec.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Executes a process command",
   usage: "exec <cmd>",
   run: async (client, message, args) => {
-  if (message.author.id !== "466778567905116170") return message.channel.send('You scrub, what made you think you\'d be able to do that??');
+  if (message.author.id !== "735237182649794571") return message.channel.send('You scrub, what made you think you\'d be able to do that??');
     childProcess.exec(args.join(' '), {},
         (err, stdout, stderr) => {
           console.log(stdout)
