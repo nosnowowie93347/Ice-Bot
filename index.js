7e2267b034dc47d114f06cb8708c53489290c4b2
nosnowowie93347 <nosnowowie@Gmail.com>
2021-04-07 14:34:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const moment = require('moment');
 const os = require('os');
 const DisTube = require('distube')
 
-
+const map = new Map();
 
 
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: false });
@@ -333,6 +333,46 @@ if (blacklist[message.author.id].state === true) return await message.reply("NOP
             db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded)
             message.channel.send(`Congrats ${message.author}, you leveled up, you are now level ${newLevel}`)
         }
+    }
+    
+    if(map.has(message.author.id)) {
+        const data = map.get(message.author.id)
+        const { lastmsg, timer } = data;
+        const diff = message.createdTimestamp - lastmsg.createdTimestamp;
+        let msgs = data.msgs
+        if(diff > 2000) {
+            clearTimeout(timer);
+            data.msgs = 1;
+            data.lastmsg = message;
+            data.timer = setTimeout(() => {
+                map.delete(message.author.id);
+            }, 5000)
+            map.set(message.author.id, data)
+        } else {
+            ++msgs;
+            if(parseInt(msgs) === 5) {
+                const rolename = 'mute' || 'muted'
+                const role = message.guild.roles.cache.find(roles => roles.name.toLowerCase() === rolename.toLowerCase())
+                message.member.roles.add(role)
+                message.channel.send(`Muted ${message.author.username}, for spamming`)
+                setTimeout(() => {
+                    message.member.roles.remove(role)
+                    message.channel.send(`Unmuted ${message.author.username}`)
+                }, 5000)
+            } else {
+                data.msgs = msgs;
+                map.set(message.author.id, data)
+            }
+        }
+    } else {
+        let remove = setTimeout(() => {
+            map.delete(message.author.id);
+        }, 5000)
+        map.set(message.author.id, {
+            msgs: 1,
+            lastmsg: message,
+            timer: remove
+        })
     }
 });
 client.on("messageDelete", async (message) => {
