80fdaeb81f742c5ebb5053382dde0bbf7be81d67
nosnowowie93347 <nosnowowie@Gmail.com>
2021-05-27 11:40:27 -0700

    Add files via upload

---
+++ commands/fun/yttogether.js
@@ -0,0 +1,38 @@
+const discord = require('discord.js')
+const fetch = require('node-fetch')
+
+module.exports = {
+    name: 'yt-t',
+    aliases: [],
+    category: "fun",
+    description: "starts a youtube together stream",
+    run: async (client, message, args) => {
+
+    let channel = message.member.voice.channel;
+    if(!channel) return message.channel.send("You have to be in a vc")
+
+    fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+        method: "POST",
+        body: JSON.stringify({
+            max_age: 86400,
+            max_uses: 0,
+            target_application_id: "695370105307136132",
+            target_type: 2,
+            temporary: false,
+            validate: null
+        }),
+        headers: {
+            "Authorization": `Bot ${client.token}`,
+            "Content-Type": "application/json"
+        }
+    })
+    
+    .then(res => res.json())
+    .then(invite => {
+        if(!invite.code) return message.channel.send("Sadly i cant start a yt together")
+        const e = new discord.MessageEmbed()
+        .setDescription(`[Click me](https://discord.com/invite/${invite.code})`)
+        message.channel.send(e)
+    })
+}
+}
