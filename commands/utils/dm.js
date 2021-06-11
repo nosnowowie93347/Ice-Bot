c60f41dfa9615b4e43754fa7e81155d91e41b0c2
nosnowowie93347 <nosnowowie@Gmail.com>
2021-06-11 11:47:30 -0700

    Update dm.js

--- commands/utils/dm.js
+++ commands/utils/dm.js
@@ -1,10 +1,15 @@
 const Discord = require("discord.js")
+const cooldown = new Set();
+
 module.exports = {
   name: "dm",
   aliases:  ["pm"],
   description: "Direct message anyone in the server!",
   category: "utils",
   run: async (client, message, args) => {
+    if (cooldown.has(message.author.id)) {
+      await message.reply("Please wait 15s to use this command.")
+    } else {
   let user =
       message.mentions.members.first() ||
       message.guild.members.cache.get(args[0]);
@@ -23,5 +28,10 @@ module.exports = {
       .send(args.slice(1).join(" "))
       .catch(() => message.channel.send("That user could not be DMed!"))
       .then(() => message.channel.send({embed}));
+      cooldown.add(message.author.id)
+      setTimeout(() => {
+            cooldown.delete(message.author.id)
+        }, 15000);
+  }
   }
 }
